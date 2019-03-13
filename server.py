7a1d9fb6da50230ebf28534a020b00ecd6ce755f
Noah Schumacher <noahschumacher14@gmail.com>
2019-03-13 11:47:34 -0700

    Working page with prediciton

--- server.py
+++ server.py
@@ -17,6 +17,19 @@ def mpg():
 	return render_template('mpg.html')
 
 
+model = pickle.load(open('linreg.p', 'rb'))
+@app.route('/inference', methods = ['POST'])
+def inference():
+	req = request.get_json()
+	print(req)
+
+	c,h,w = req['cylinders'], req['horsepower'], req['weight']
+
+	prediction = list(model.predict([[c,h,w]]))
+
+	return jsonify({'c':c, 'h':h, 'w':w, 'prediction':prediction[0]})
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=3333, debug=True)
 
---
+++ static/mpg.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+	console.log('Document is ready')
+
+	$("#inference").click(async function(){
+		console.log('button was clicked')
+
+		const cylinders = parseFloat($('#cylinders').val());
+		const horsepower = parseFloat($('#horsepower').val());
+		const weight = parseFloat($('#weight').val());
+
+		const data = {
+			cylinders,
+			horsepower,
+			weight
+		}
+
+		console.log(data)
+
+		const response = await $.ajax('/inference', {
+			data: JSON.stringify(data),
+			method: "post",
+			contentType:'application/json'
+		})
+
+		console.log(response)
+		$('#mpg').val(response.prediction)
+
+	})
+})
