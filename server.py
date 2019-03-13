086c50655e7439a4252182aaeb9c35a516cc9db8
Noah Schumacher <noahschumacher14@gmail.com>
2019-03-13 10:42:21 -0700

    mpg page formatting and button adding

--- server.py
+++ server.py
@@ -8,9 +8,14 @@ app = Flask(__name__)
 
 
 @app.route('/', methods = ['GET'])
-
 def home():
 	return render_template('home.html')
+
+
+@app.route('/mpg', methods = ['GET'])
+def mpg():
+	return render_template('mpg.html')
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=3333, debug=True)
